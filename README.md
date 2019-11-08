775dc1b3bdc64fe133263d2691afd8b2f3a3276b
ted <ted@timmons.me>
2019-11-08 11:36:02 -0800

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+Bits of code to explore the Helium IOT ecosystem. Shared with [community.helium.com](https://community.helium.com) and the related Slack, but mostly to scratch my own itch.
+
+PRs and comments welcome. I have more scripts I'll share eventually.
+
+Unless otherwise mentioned, code is [MIT licensed](LICENSE).
---
+++ nearby.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# copyright 2019, MIT License.
+
+# this script needs python3.6 or newer because of the "f-string" syntax,
+# aka PEP 498's literal string interpolation. you'll also need geopy and requests:
+# pip3 install geopy requests
+
+from geopy.distance import geodesic as geo
+import re
+import sys
+import requests
+
+# helper funcs
+def loc(d, prefix=''):
+  return (d[f'{prefix}lat'], d[f'{prefix}lng'])
+def normalize_name(n):
+  return re.sub(r'\W', '-', n.lower())
+
+homeid = None
+if len(sys.argv) > 1:
+  homeid = normalize_name(' '.join(sys.argv[1:]))
+else:
+  print("give your hotspot id (one-two-three) as the argument. spaces or dashes are fine.")
+
+print(f"normalized name: {homeid}")
+
+home = None
+hotspot = {}
+r = requests.get('https://network.helium.com/fetchHotspots')
+for h in r.json():
+  # save them by name
+  stdname = normalize_name(h['name'])
+  hotspot[stdname] = h
+  if stdname == homeid:
+    home = h
+
+if not home:
+  print(f"eep, we didn't find your hotspot name ({homeid}).")
+  sys.exit(-1)
+
+
+# now compare them to see if they're local
+print(f"dist score name")
+for h_id,h in hotspot.items():
+  dist = geo( loc(home), loc(h) ).miles
+  if dist < 30:
+    print(f"{dist:4.1f} {int(h['score']*100):5} {h['name']}")
+
