db7d79a87bab604b325679fbfdbf34b0f7f35059
ted <ted@timmons.me>
2019-11-21 08:32:23 -0800

    add witness report

--- nearby.py
+++ nearby.py
@@ -10,6 +10,7 @@ from geopy.distance import geodesic as geo
 import re
 import sys
 import requests
+import datetime
 
 # helper funcs
 def loc(d, prefix=''):
@@ -46,4 +47,16 @@ for h_id,h in hotspot.items():
   dist = geo( loc(home), loc(h) ).miles
   if dist < 30:
     print(f"{dist:4.1f} {int(h['score']*100):5} {h['name']}")
+
+# now pull peeps who have witnessed us: https://tedder.me/lols/witness-me-2.gif
+witret = requests.get(f'https://alamo.helium.foundation/api/witnesses/{home["address"]}')
+data = witret.json().get('data')
+if len(data):
+  print(f"\nhotspot name         count  recent time")
+  for h in data:
+    # h = dict of pocs that saw us
+    print(f"{h['name']:20} {sum(h['hist'].values()):5}  {datetime.datetime.fromtimestamp(h['recent_time']/1000**3).isoformat()}")
+
+
+
 
