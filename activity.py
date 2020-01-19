6c8858f66b46c41f8226df35bc3f59414f85c659
ted <ted@timmons.me>
2020-01-18 17:00:32 -0800

    refactor, add witness details
    * refactor into functions. steal more from nearby.py (it's almost time to turn these into a class)
    * expand the witness info
    * format output

--- activity.py
+++ activity.py
@@ -7,69 +7,108 @@
 # pip3 install requests
 
 
+from geopy.distance import geodesic as geo
 import re
 import sys
 import requests
 import datetime
 
-def normalize_name(n):
-  return re.sub(r'\W', '-', n.lower())
+def safe_name_from_address(hotspot_address_map, address):
+  h = hotspot_address_map.get(address)
+  if not h: return ''
+  return normalize_name(h['name'])
 
-homeid = None
-if len(sys.argv) > 1:
-  homeid = normalize_name(' '.join(sys.argv[1:]))
-else:
-  print("give your hotspot id (one-two-three) as the argument. spaces or dashes are fine.")
+def get_challenge(home, challenge_id, hotspot_address_map):
+  hotspot_address = home['address']
+  challenge_activity = requests.get(f'https://explorer.helium.foundation/api/challenges/{challenge_id}')
+  path = challenge_activity.json()['data']['pathElements']
+  print("  result     witnesses  distance target                    receipt?")
+  for i,elem in enumerate(path):
+    if home['address'] == elem['address']:
+      # even if these two are the same the lat/lon may not be equal. yay for floats and rounding.
+      dist_txt = "   ----"
+    else:
+      dist = geo( loc(home), loc(elem) ).miles
+      dist_txt = f"{dist:7.2f}"
+    tgt = safe_name_from_address(hotspot_address_map, elem['address'])
+    rct_tgt = safe_name_from_address(hotspot_address_map, elem['receipt'].get('address'))
+    receipt_label = "not received"
+    if rct_tgt == tgt:
+      receipt_label = "received"
 
-print(f"normalized name: {homeid}")
+    print(f"  {elem['result']:10} {len(elem['witnesses']):5}      {dist_txt}  {tgt:25} {receipt_label}")
 
-home = None
-hotspot = {}
-r = requests.get('https://network.helium.com/fetchHotspots')
-for h in r.json():
-  # save them by name
-  stdname = normalize_name(h['name'])
-  hotspot[stdname] = h
-  if stdname == homeid:
-    home = h
-
-if not home:
-  print(f"eep, we didn't find your hotspot name ({homeid}).")
-  sys.exit(-1)
-
-
-activity = requests.get(f'https://explorer.helium.foundation/api/hotspots/{home["address"]}/activity')
-data = activity.json().get('data')
-if len(data):
+def get_activity(home, hotspot_map):
+  hotspot_address = home['address']
+  activity = requests.get(f'https://explorer.helium.foundation/api/hotspots/{hotspot_address}/activity')
+  data = activity.json().get('data')
+  if not len(data):
+    print("no activity in the API for you.")
+    return
+  
   for a in reversed(data):
     reward_type = a['reward_type']
     witness_id = a['poc_witness_challenge_id']
     challenge_req_hash = a['poc_req_txn_hash']
     challenge_hash = a['poc_rx_txn_hash']
-
+    id_spacing = ''
+  
     if reward_type is not None and reward_type.startswith('poc'):
       block_id = a['reward_block_height']
       reward_amount = a['reward_amount']/100000000
-      reward_time = datetime.datetime.fromtimestamp(a['reward_block_time']).isoformat()
-
+      reward_time = datetime.datetime.fromtimestamp(a['reward_block_time']).isoformat(timespec='seconds')
+  
       if reward_type == 'poc_challengers':
-        print(f"{reward_time}: Block {block_id} - Mined {reward_amount} - Challenger")
+        print(f"{reward_time}: Block {block_id} - {id_spacing:9} Mined {reward_amount} - Challenger")
       elif reward_type == 'poc_challengees':
-        print(f"{reward_time}: Block {block_id} - Mined {reward_amount} - Challengee")
+        print(f"{reward_time}: Block {block_id} - {id_spacing:9} Mined {reward_amount} - Challengee")
       elif reward_type == 'poc_witnesses':
-        print(f"{reward_time}: Block {block_id} - Mined {reward_amount} - Witness")
+        print(f"{reward_time}: Block {block_id} - {id_spacing:9} Mined {reward_amount} - Witness")
     elif witness_id is not None:
-      witness_time = datetime.datetime.fromtimestamp(a['poc_rx_txn_block_time']).isoformat()
+      witness_time = datetime.datetime.fromtimestamp(a['poc_rx_txn_block_time']).isoformat(timespec='seconds')
       block_id = a['poc_rx_txn_block_height']
-      print(f"{witness_time}: Block {block_id} - Challenge Witnessed {witness_id}")
+      print(f"{witness_time}: Block {block_id} - {witness_id:7} - Challenge Witnessed")
     elif challenge_req_hash is not None:
-      challenge_time = datetime.datetime.fromtimestamp(a['poc_req_txn_block_time']).isoformat()
+      challenge_time = datetime.datetime.fromtimestamp(a['poc_req_txn_block_time']).isoformat(timespec='seconds')
       block_id = a['poc_req_txn_block_height']
-      print(f"{challenge_time}: Block {block_id} - Challenge Constructed")
+      print(f"{challenge_time}: Block {block_id} - {id_spacing:9} Challenge Constructed")
     elif challenge_hash is not None:
-      challenge_time = datetime.datetime.fromtimestamp(a['poc_rx_txn_block_time']).isoformat()
+      challenge_time = datetime.datetime.fromtimestamp(a['poc_rx_txn_block_time']).isoformat(timespec='seconds')
       block_id = a['poc_rx_txn_block_height']
       challenge_id = a['poc_rx_challenge_id']
-      print(f"{challenge_time}: Block {block_id} - Challenge Success {challenge_id}")
+      print(f"{challenge_time}: Block {block_id} - {challenge_id:7} - Challenge Success")
+      get_challenge(home, challenge_id, hotspot_map)
     else:
       print("Unknown")
+
+def loc(d, prefix=''):
+  return (d[f'{prefix}lat'], d[f'{prefix}lng'])
+def normalize_name(n):
+  return re.sub(r'\W', '-', n.lower())
+
+def get_hotspot_address(hotspot_name):
+  normalized = normalize_name(hotspot_name)
+  home = None
+  hmap = {}
+  r = requests.get('https://network.helium.com/fetchHotspots')
+  for h in r.json():
+    # save them by name
+    stdname = normalize_name(h['name'])
+    if stdname == normalized:
+      home = h
+    hmap[h['address']] = h
+  return (home, hmap)
+
+hotspot_raw_name = ' '.join(sys.argv[1:])
+if len(sys.argv) > 1:
+  (home, hotspot_address_map) = get_hotspot_address(' '.join(sys.argv[1:]))
+else:
+  print("give your hotspot id (one-two-three) as the argument. spaces or dashes are fine.")
+  sys.exit(-1)
+
+if not home:
+  print(f"eep, we didn't find your hotspot name ({hotspot_raw_name}).")
+  sys.exit(-1)
+
+get_activity(home, hotspot_address_map)
+
