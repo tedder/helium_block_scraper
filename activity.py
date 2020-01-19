71d1f3479c94163327b0127247d0a1a5b9d79957
Kelly Byrd <kbyrd@proofpoint.com>
2020-01-19 09:35:07 -0800

    Quick PEP8-ish reformating pass
    Let SublimeLinter and isort guide me on some reformatting, but
    intentionally ignored things like 2 vs 4 spaces for indents and
    too many statements on a line.
    
    Also added `__main__` check, but it's not really needed right now

--- activity.py
+++ activity.py
@@ -4,32 +4,32 @@
 
 # this script needs python3.6 or newer because of the "f-string" syntax,
 # aka PEP 498's literal string interpolation. you'll also requests:
-# pip3 install requests
-
-
-from datetime import timezone as timezone
-from geopy.distance import geodesic as geo
+# pip3 install geopy requests
 import re
 import sys
+from datetime import datetime, timezone
+
 import requests
-import datetime
+from geopy.distance import geodesic as geo
+
 
 def safe_name_from_address(hotspot_address_map, address):
   h = hotspot_address_map.get(address)
   if not h: return ''
   return normalize_name(h['name'])
 
+
 def get_challenge(home, challenge_id, hotspot_address_map):
   hotspot_address = home['address']
   challenge_activity = requests.get(f'https://explorer.helium.foundation/api/challenges/{challenge_id}')
   path = challenge_activity.json()['data']['pathElements']
   print("  result     witnesses  distance path     target                    receipt?")
-  for i,elem in enumerate(path):
+  for i, elem in enumerate(path):
     if home['address'] == elem['address']:
       # even if these two are the same the lat/lon may not be equal. yay for floats and rounding.
       dist_txt = "   ----"
     else:
-      dist = geo( loc(home), loc(elem) ).miles
+      dist = geo(loc(home), loc(elem)).miles
       dist_txt = f"{dist:7.2f}"
     tgt = safe_name_from_address(hotspot_address_map, elem['address'])
     rct_tgt = safe_name_from_address(hotspot_address_map, elem['receipt'].get('address'))
@@ -38,6 +38,7 @@ def get_challenge(home, challenge_id, hotspot_address_map):
       receipt_label = "received"
 
     print(f"  {elem['result']:10} {len(elem['witnesses']):5}      {dist_txt}  {elem['receipt'].get('origin', ''):8} {tgt:25} {receipt_label}")
+
 
 def get_activity(home, hotspot_map):
   hotspot_address = home['address']
@@ -56,7 +57,7 @@ def get_activity(home, hotspot_map):
 
     if reward_type is not None and reward_type.startswith('poc'):
       block_id = a['reward_block_height']
-      reward_amount = a['reward_amount']/100000000
+      reward_amount = a['reward_amount'] / 100000000
       reward_time = format_time(a['reward_block_time'])
 
       if reward_type == 'poc_challengers':
@@ -82,18 +83,23 @@ def get_activity(home, hotspot_map):
     else:
       print("Unknown")
 
+
 # Given a unix timestamp format it into an RFC3339 string in the  local time zone
 def format_time(timestamp):
   return (
-    datetime.datetime.fromtimestamp(timestamp, tz=timezone.utc)
-    .astimezone()
-    .isoformat(timespec='seconds')
+      datetime.fromtimestamp(timestamp, tz=timezone.utc)
+      .astimezone()
+      .isoformat(timespec='seconds')
   )
+
 
 def loc(d, prefix=''):
   return (d[f'{prefix}lat'], d[f'{prefix}lng'])
+
+
 def normalize_name(n):
   return re.sub(r'\W', '-', n.lower())
+
 
 def get_hotspot_address(hotspot_name):
   normalized = normalize_name(hotspot_name)
@@ -108,16 +114,22 @@ def get_hotspot_address(hotspot_name):
     hmap[h['address']] = h
   return (home, hmap)
 
-hotspot_raw_name = ' '.join(sys.argv[1:])
-if len(sys.argv) > 1:
-  (home, hotspot_address_map) = get_hotspot_address(' '.join(sys.argv[1:]))
-else:
-  print("give your hotspot id (one-two-three) as the argument. spaces or dashes are fine.")
-  sys.exit(-1)
 
-if not home:
-  print(f"eep, we didn't find your hotspot name ({hotspot_raw_name}).")
-  sys.exit(-1)
+# TODO: redo this a bit to take some params
+def main():
+  hotspot_raw_name = ' '.join(sys.argv[1:])
+  if len(sys.argv) > 1:
+    (home, hotspot_address_map) = get_hotspot_address(' '.join(sys.argv[1:]))
+  else:
+    print("give your hotspot id (one-two-three) as the argument. spaces or dashes are fine.")
+    sys.exit(-1)
 
-get_activity(home, hotspot_address_map)
+  if not home:
+    print(f"eep, we didn't find your hotspot name ({hotspot_raw_name}).")
+    sys.exit(-1)
 
+  get_activity(home, hotspot_address_map)
+
+
+if __name__ == '__main__':
+  main()
--- nearby.py
+++ nearby.py
@@ -5,67 +5,75 @@
 # this script needs python3.6 or newer because of the "f-string" syntax,
 # aka PEP 498's literal string interpolation. you'll also need geopy and requests:
 # pip3 install geopy requests
-
-from geopy.distance import geodesic as geo
+import datetime
 import re
 import sys
+
 import requests
-import datetime
+from geopy.distance import geodesic as geo
+
 
 # helper funcs
 def loc(d, prefix=''):
   return (d[f'{prefix}lat'], d[f'{prefix}lng'])
+
+
 def normalize_name(n):
   return re.sub(r'\W', '-', n.lower())
 
-homeid = None
-if len(sys.argv) > 1:
-  homeid = normalize_name(' '.join(sys.argv[1:]))
-else:
-  print("give your hotspot id (one-two-three) as the argument. spaces or dashes are fine.")
 
-print(f"normalized name: {homeid}")
+# TODO: redo this a bit to take some params
+def main():
+  homeid = None
+  if len(sys.argv) > 1:
+    homeid = normalize_name(' '.join(sys.argv[1:]))
+  else:
+    print("give your hotspot id (one-two-three) as the argument. spaces or dashes are fine.")
 
-home = None
-hotspot = {}
-r = requests.get('https://network.helium.com/fetchHotspots')
-for h in r.json():
-  # save them by name
-  stdname = normalize_name(h['name'])
-  hotspot[stdname] = h
-  if stdname == homeid:
-    home = h
+  print(f"normalized name: {homeid}")
 
-if not home:
-  print(f"eep, we didn't find your hotspot name ({homeid}).")
-  sys.exit(-1)
+  home = None
+  hotspot = {}
+  r = requests.get('https://network.helium.com/fetchHotspots')
+  for h in r.json():
+    # save them by name
+    stdname = normalize_name(h['name'])
+    hotspot[stdname] = h
+    if stdname == homeid:
+      home = h
 
-hotspots = []
+  if not home:
+    print(f"eep, we didn't find your hotspot name ({homeid}).")
+    sys.exit(-1)
 
-# now compare them to see if they're local
-print(f"dist score name")
-for h_id,h in hotspot.items():
-  dist = geo( loc(home), loc(h) ).miles
-  if dist < 30:
-    score = int(h['score']*100)
-    coord = f"{h['lat']:3.4f}, {h['lng']:3.4f}"
-    name = h['name']
-    result = (dist, score, name, coord)
-    hotspots.append(result)
+  hotspots = []
 
-for result in reversed(sorted(hotspots, key=lambda x: x[0])):
-  print(f"{result[0]:4.1f} {result[1]:5} {result[2]:30} {result[3]}")
+  # now compare them to see if they're local
+  print(f"dist score name")
+  for h_id, h in hotspot.items():
+    dist = geo(loc(home), loc(h)).miles
+    if dist < 30:
+      score = int(h['score'] * 100)
+      coord = f"{h['lat']:3.4f}, {h['lng']:3.4f}"
+      name = h['name']
+      result = (dist, score, name, coord)
+      hotspots.append(result)
 
-# now pull peeps who have witnessed us: https://tedder.me/lols/witness-me-2.gif
-witret = requests.get(f'https://explorer.helium.foundation/api/witnesses/{home["address"]}')
-data = witret.json().get('data')
-if len(data):
-  print(f"\nhotspot name                   count    rssi  witness time")
-  for h in sorted(data, key=lambda x: x.get('recent_time')):
-    # h = dict of pocs that saw us
-    hist = h['hist']
-    rssi = max(hist, key=hist.get)
-    count = sum(hist.values())
-    print(f"{h['name']:25} {count:10}   {rssi:5}  {datetime.datetime.fromtimestamp(h['recent_time']/1000**3).isoformat(timespec='minutes')}")
+  for result in reversed(sorted(hotspots, key=lambda x: x[0])):
+    print(f"{result[0]:4.1f} {result[1]:5} {result[2]:30} {result[3]}")
+
+  # now pull peeps who have witnessed us: https://tedder.me/lols/witness-me-2.gif
+  witret = requests.get(f'https://explorer.helium.foundation/api/witnesses/{home["address"]}')
+  data = witret.json().get('data')
+  if len(data):
+    print(f"\nhotspot name                   count    rssi  witness time")
+    for h in sorted(data, key=lambda x: x.get('recent_time')):
+      # h = dict of pocs that saw us
+      hist = h['hist']
+      rssi = max(hist, key=hist.get)
+      count = sum(hist.values())
+      print(f"{h['name']:25} {count:10}   {rssi:5}  {datetime.datetime.fromtimestamp(h['recent_time']/1000**3).isoformat(timespec='minutes')}")
 
 
+if __name__ == '__main__':
+  main()
